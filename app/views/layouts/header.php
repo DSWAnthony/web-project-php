683be05a14318ed6390e0b7be05f7f74dc1d9b99
cristian20405 <cristiancruzremuzgo@gmail.com>
2025-04-13 20:22:42 -0500

    correccion urls

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -10,13 +10,13 @@ include '../../config/conexion.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title><?php echo isset($title) ? $title : 'ELITE'; ?></title> 
 
-    <link href="/web-project-php/public/css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../../public/css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- jQuery (necesario para AJAX) -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <!-- Archivo JavaScript personalizado -->
-    <script src="/web-project-php/public/js/provedor.js"></script>
+    <script src="../../../public/js/provedor.js"></script>
     <link href="../../../public/css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     
 </head>
