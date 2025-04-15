fee57a6a8b37293ffab76c3e5173a24dd6d719c3
jaren <jarensantos10@gmail.com>
2025-04-14 20:28:04 -0500

    footer grupo2

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -1,5 +1,6 @@
     <!-- Footer -->
     <footer class="bg-dark text-white text-center py-3 mt-5">
+        <p>GRUPO 2</p>
         <p>&copy; 2025 ZAPATILLAS ELITE SAC.</p>
         <script src="../../../public/css/bootstrap/js/bootstrap.min.js"></script>
     </footer>
